c5d829ff94818980b274c0da1a15267e3bad8237
Saim Abbas <saimabbas220022@gmail.com>
2022-04-21 20:18:21 +0500

    Checkout Page

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import React from "react";
-import Page from "./views/MintConnect";
+import Page from "./views/Checkout";
 const App = () => {
   return <Page />;
 };
--- src/views/Checkout.jsx
+++ src/views/Checkout.jsx
@@ -1,4 +1,17 @@
 import React from 'react'
+import BlobPurple from "../assets/img/blob-purple.svg";
+import HeroBgLight from "./../assets/img/hero-bg-light.png";
+import QRcode from "./../assets/img/qrcode.png";
+import Wallet from "./../assets/img/wallet.png";
+import SumImg from "./../assets/img/checkoutimg.png";
+import Logo from "../assets/img/Logo";
+import Instagram from "../assets/icons/Instagram";
+import TwitterIcon from "../assets/icons/TwitterIcon";
+import Discord from "../assets/icons/Discord";
+import RefreshIcon from "../assets/icons/RefreshIcon";
+import MessagesIcon from "../assets/icons/MessagesIcon";
+import MoonIcon from "../assets/icons/MoonIcon";
+import WalletIcon from "../assets/icons/WalletIcon";
 
 const Checkout = () => {
     return (
@@ -64,7 +77,56 @@ const Checkout = () => {
                                     <h2>Checkout</h2>
                                     <img src={BlobPurple} alt="BlobPurple" />
                                 </div>
+                            </div>
+                            <div className="checkoutmainpage">
+                                <div className="checkoutleft">
+                                    <h4>Process & Complete</h4>
+                                    <div className="coleftimg">
+                                        <p>Transfer the amount to the following wallet and wait for the confirmation message</p>
+                                        <img src={QRcode} alt="" />
+                                    </div>
+                                    <div className="colwallet">
+                                        <h5>Wallet address</h5>
+                                        <p>Transfer to Wallet Address</p>
+                                        <div className="colwalbtn">
+                                            <div className="colwalp">
+                                                <img src={Wallet} alt="" />
+                                                <p>1vtWSBkSF3wiuy1fN2UPx1Zn5nN2jszSf</p>
+                                            </div>
+                                            <button>Copy</button>
+                                        </div>
+                                    </div>
 
+                                </div>
+                                <div className="checkoutright">
+                                    <h4>Summary</h4>
+                                    <div className="sumimgtext">
+                                        <img src={SumImg} alt="" />
+                                        <div className="sumtext">
+                                            <h6>Season 1</h6>
+                                            <p>Tier 3 (The Inner Circle)</p>
+                                            <span>View Item</span>
+                                        </div>
+                                    </div>
+                                    <div className="checkouttotal">
+                                        <div className="cotinner">
+                                            <p>Season 1</p>
+                                            <h6>0.6 ETH</h6>
+                                        </div>
+                                        <div className="cotinner">
+                                            <p>Gas</p>
+                                            <h6>0.02 ETH</h6>
+                                        </div>
+                                        <div className="cottotal">
+                                            <p>Total</p>
+                                            <h6>0.62 ETH</h6>
+                                        </div>
+                                    </div>
+                                    <div className="cosub">
+                                        <input type="text" placeholder='Promo code' />
+                                        <button>Submit</button>
+                                    </div>
+                                </div>
                             </div>
                         </div>
                     </div>
