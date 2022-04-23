b08f739801c91e96ee742b504317da95f409e725
Saim Abbas <saimabbas220022@gmail.com>
2022-04-23 14:15:38 +0500

    Router

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,7 @@
 import React from "react";
-import Page from "./views/Checkout";
-import { Routes, Route, Switch } from "react-router-dom";
+import { BrowserRouter as Switch, Routes, Route } from 'react-router-dom';
 import PrizePools from "./views/PrizePools";
+import Home from "./views/Home";
 import Checkout from "./views/Checkout";
 import Collections from "./views/Collections";
 import ComingSoon from "./views/ComingSoon";
@@ -14,23 +14,21 @@ import Subscribe from "./views/Subscribe";
 const App = () => {
   return (
     <>
-      <Page />
-      <Routes>
-        <Route exact path="./views/Checkout" element={<Checkout />} />
-        <Route exact path="./views/collections" element={<Collections />} />
-        <Route exact path="./views/team" element={<Team />} />
-        <Route exact path="./views/comingson" element={<ComingSoon />} />
-        <Route
-          exact
-          path="./views/comingsoondark"
-          element={<ComingSoonDark />}
-        />
-        <Route exact path="./views/landingpage" element={<LandingPage />} />
-        <Route exact path="./views/roadmap" element={<Roadmap />} />
-        <Route exact path="./views/prizetools" element={<PrizePools />} />
-        <Route exact path="./views/subscribe" element={<Subscribe />} />
-        <Route exact path="./views/vault" element={<Vault />} />
-      </Routes>
+      <Switch>
+        <Routes>
+          <Route exact path="/" element={<Home />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/collections" element={<Collections />} />
+          <Route path="/team" element={<Team />} />
+          <Route path="/comingsoon" element={<ComingSoon />} />
+          <Route path="/landingpage" element={<LandingPage />} />
+          <Route path="/roadmap" element={<Roadmap />} />
+          <Route path="/prizetools" element={<PrizePools />} />
+          <Route path="/subscribe" element={<Subscribe />} />
+          <Route path="/vault" element={<Vault />} />
+          <Route path="/comingsoondark" element={<ComingSoonDark />} />
+        </Routes>
+      </Switch>
     </>
   );
 };
--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { BrowserRouter as Router, Switch, Route, Link } from "react-router-dom";
+import { BrowserRouter } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./scss/main.css";
@@ -9,9 +9,9 @@ import reportWebVitals from "./reportWebVitals";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <Router>
-      <App />
-    </Router>
+    {/* <BrowserRouter> */}
+    <App />
+    {/* </BrowserRouter> */}
   </React.StrictMode>
 );
 reportWebVitals();
