a2a447477a348cd3c98c5fa37293abf73db65bd6
Saim Abbas <saimabbas220022@gmail.com>
2022-04-24 19:56:21 +0500

    Roadmap

--- src/views/Roadmap.jsx
+++ src/views/Roadmap.jsx
@@ -1,65 +1,20 @@
 import React from "react";
 import BlobPurple from "../assets/img/blob-purple.svg";
 import HeroBgLight from "./../assets/img/hero-bg-light.png";
-import Logo from "../assets/img/Logo";
-import Instagram from "../assets/icons/Instagram";
-import TwitterIcon from "../assets/icons/TwitterIcon";
-import Discord from "../assets/icons/Discord";
-import RefreshIcon from "../assets/icons/RefreshIcon";
-import MessagesIcon from "../assets/icons/MessagesIcon";
-import MoonIcon from "../assets/icons/MoonIcon";
-import WalletIcon from "../assets/icons/WalletIcon";
+import Header from "../components/modules/Header";
+import Footer from "../components/modules/Footer";
+import { Mousewheel, FreeMode } from "swiper";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/free-mode";
+import { MdDone } from "react-icons/md";
 
 const Roadmap = () => {
   return (
     <div className="app light-theme">
       {/* Header */}
 
-      <header>
-        <div className="box">
-          <div className="header-content">
-            <div className="header-left">
-              <Logo color="#1F194D" />
-              {/* <img src={LogoLight} alt="LogoLight" /> */}
-              <ul>
-                <li>
-                  <a href="#">
-                    <b>P</b>rize pool
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <b>S</b>ubscribe
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <b>R</b>oadmap
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <b>U</b>pcoming Drops
-                  </a>
-                </li>
-                <li>
-                  <a href="#">
-                    <b>M</b>eet the Team
-                  </a>
-                </li>
-              </ul>
-            </div>
-            <div className="header-right">
-              <MessagesIcon color="#1F194D" />
-              <MoonIcon color="#1F194D" />
-              <button className="light-blue-btn-filled btn-sm-ds">
-                {" "}
-                <WalletIcon color="#195BFF" /> Wallet
-              </button>
-            </div>
-          </div>
-        </div>
-      </header>
+      <Header />
       <div className="roadmapmainpage">
         {/* Roadmap Section */}
         <section className="roadmapmain">
@@ -68,66 +23,158 @@ const Roadmap = () => {
             src={HeroBgLight}
             alt="HeroBgLight"
           />
-          <div className="box">
-            <div className="letushelp-content hiwsection">
-              <div className="section-heading">
-                <div className="sh-heading">
-                  <h2>Roadmap</h2>
-                  <img src={BlobPurple} alt="BlobPurple" />
+          <div className="app-page">
+            <div className="box">
+              <div className="letushelp-content hiwsection">
+                <div className="section-heading">
+                  <div className="sh-heading">
+                    <h2>Roadmap</h2>
+                    <img src={BlobPurple} alt="BlobPurple" />
+                  </div>
                 </div>
-                <div className="mainroadmap"></div>
+                <div className="roadmap-content">
+                  <div className="roadmap-swiper">
+                    <Swiper
+                      modules={[FreeMode, Mousewheel]}
+                      className="mySwiper"
+                      slidesPerView={"auto"}
+                      freeMode={true}
+                      mousewheel={true}
+                    >
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-startpoint">
+                            <span>
+                              <p>S1</p>
+                            </span>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-step roadmap-done">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-step-dot">
+                              <MdDone />
+                              <div className="roadmap-step-dot-inner-1"></div>
+                              <div className="roadmap-step-dot-inner-2"></div>
+                              <p className="roadmap-step-text-bottom">
+                                UI and Site Development
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-step roadmap-current">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-step-dot">
+                              <MdDone />
+                              <div className="roadmap-step-dot-inner-1"></div>
+                              <div className="roadmap-step-dot-inner-2"></div>
+                              <p className="roadmap-step-text-top">
+                                Advisory and team build out
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-step">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-step-dot">
+                              <MdDone />
+                              <div className="roadmap-step-dot-inner-1"></div>
+                              <div className="roadmap-step-dot-inner-2"></div>
+                              <p className="roadmap-step-text-bottom">
+                                Community structure
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-step">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-step-dot">
+                              <MdDone />
+                              <div className="roadmap-step-dot-inner-1"></div>
+                              <div className="roadmap-step-dot-inner-2"></div>
+                              <p className="roadmap-step-text-top">Site Drop</p>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-step">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-step-dot">
+                              <MdDone />
+                              <div className="roadmap-step-dot-inner-1"></div>
+                              <div className="roadmap-step-dot-inner-2"></div>
+                              <p className="roadmap-step-text-bottom">
+                                Voting system implementation
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-step">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-step-dot">
+                              <MdDone />
+                              <div className="roadmap-step-dot-inner-1"></div>
+                              <div className="roadmap-step-dot-inner-2"></div>
+                              <p className="roadmap-step-text-top">
+                                Sample Text
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-step">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-step-dot">
+                              <MdDone />
+                              <div className="roadmap-step-dot-inner-1"></div>
+                              <div className="roadmap-step-dot-inner-2"></div>
+                              <p className="roadmap-step-text-bottom">
+                                Sample Text
+                              </p>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                      <SwiperSlide>
+                        <div className="swiper-slide-content">
+                          <div className="roadmap-endpoint-box">
+                            <div className="roadmap-step-line"></div>
+                            <div className="roadmap-endpoint">
+                              <span>
+                                <p>S1</p>
+                              </span>
+                            </div>
+                          </div>
+                        </div>
+                      </SwiperSlide>
+                    </Swiper>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </section>
       </div>
       {/* Footer */}
-      <footer>
-        <div className="box">
-          <div className="footer-content">
-            <div className="footer-top">
-              <div className="footer-left">
-                <Logo color="#fff" />
-                <p>Follow us on social media</p>
-                <div className="fl-icons-grid">
-                  <Instagram color="#fff" />
-                  <TwitterIcon color="#fff" />
-                  <Discord color="#fff" />
-                </div>
-              </div>
-              <div className="footer-center">
-                <h6>The Whitelist Info</h6>
-                <div className="fc-grid">
-                  <a href="#">Subscribe</a>
-                  <a href="#">Upcoming Drops </a>
-                  <a href="#">Roadmap</a>
-                  <a href="#">
-                    <b>Meet the Team</b>
-                  </a>
-                  <a href="#">How it Works</a>
-                </div>
-              </div>
-              <div className="footer-right">
-                <h6>Join Newsletter</h6>
-                <p>
-                  Subscribe our newsletter to get latest <Logo color="#fff" />
-                  updates
-                </p>
-                <div className="fr-subscribe-input-box">
-                  <input type="text" placeholder="Enter your Email " />
-                  <button>Subscribe</button>
-                </div>
-              </div>
-            </div>
-          </div>
-        </div>
-        <div className="footer-bottom">
-          <p>Copyright © 2022. All rights reserved</p>
-        </div>
-        <div className="more-btn">
-          More <RefreshIcon color="#fff" />
-        </div>
-      </footer>
+      <Footer />
     </div>
   );
 };
