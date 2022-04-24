6693376575ffc41e691fe3b247979db38307c021
Saim Abbas <saimabbas220022@gmail.com>
2022-04-25 02:49:30 +0500

    Final Push of the day

--- src/views/Links.jsx
+++ src/views/Links.jsx
@@ -4,20 +4,48 @@ import { Link } from "react-router-dom";
 const Links = () => {
   return (
     <div className="links-page">
-      <Link to="/home">home</Link>
-      <Link to="/checkout">Checkout</Link>
-      <Link to="/collections">Collections</Link>
-      <Link to="/comingsoon">comingsoon</Link>
-      <Link to="/comingsoondark">comingsoondark</Link>
-      <Link to="/team">Team</Link>
-      <Link to="/roadmap">Roadmap</Link>
-      <Link to="/howitworks">HowitWorks</Link>
-      <Link to="/mintconnect">MintConnect</Link>
-      <Link to="/mintdisconnect">MintDisconnect</Link>
-      <Link to="/landingpage">landingpage</Link>
-      <Link to="/prizepools">Prizepools</Link>
-      <Link to="/subscribe">Subscribe</Link>
-      <Link to="/vault">Vault</Link>
+      <Link to="/home">
+        1. <span>Home</span>
+      </Link>
+      <Link to="/checkout">
+        2. <span>Checkout</span>
+      </Link>
+      <Link to="/collections">
+        3. <span>Collections</span>
+      </Link>
+      <Link to="/comingsoon">
+        4. <span>Coming Soon 1</span>
+      </Link>
+      <Link to="/comingsoondark">
+        5. <span>Coming Soon 2</span>
+      </Link>
+      <Link to="/team">
+        6. <span>Team</span>
+      </Link>
+      <Link to="/roadmap">
+        7. <span>Roadmap</span>
+      </Link>
+      <Link to="/howitworks">
+        8. <span>How It Works</span>
+      </Link>
+      <Link to="/mintconnect">
+        9. <span>Mint Connect</span>
+      </Link>
+      <Link to="/mintdisconnect">
+        10. <span>Mint Disconnect</span>
+      </Link>
+      <Link to="/landingpage">
+        11. <span>Loading</span>
+      </Link>
+      <Link to="/prizepools">
+        12. <span>Prize Pools</span>
+      </Link>
+      <Link to="/subscribe">
+        13. <span>Subscribe</span>
+      </Link>
+      <Link to="/vault">
+        14. <span>Vault</span>
+      </Link>
     </div>
   );
 };
